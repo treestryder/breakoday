5bd3b3103f6835bae66b92ba2ad1fda455409a79
Nathan Hartley <3682967+treestryder@users.noreply.github.com>
2026-07-24 20:12:57 -0400

    Performed jeebies cleanup. Ignored gutspell after earlier spellchecking.

--- whitewashers/whitewash.ps1
+++ whitewashers/whitewash.ps1
@@ -1,4 +1,15 @@
 $projectFolder = Resolve-Path "$PsScriptRoot\.."
 Write-Host "Project Folder: $projectFolder"
 $book = "$projectFolder\book\The Chronicles Of Break O'day.txt"
-&"$projectFolder\whitewashers\bin\gutcheck_u\gutcheck.exe" $book > "$projectFolder\whitewashers\_gutcheck.txt"
+
+Push-Location "$projectFolder\whitewashers\bin\gutcheck_u" -Verbose
+.\gutcheck.exe $book > "$projectFolder\whitewashers\_gutcheck.txt"
+Pop-Location
+
+Push-Location "$projectFolder\whitewashers\bin\gutspell_u" -Verbose
+.\gutspell.exe $book > "$projectFolder\whitewashers\_gutspell.txt"
+Pop-Location
+
+Push-Location "$projectFolder\whitewashers\bin\jeebies_u" -Verbose
+.\jeebies.exe $book > "$projectFolder\whitewashers\_jeebies.txt"
+Pop-Location
