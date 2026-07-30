b8bf23e1562de5c67b45f65e822d670d55b8c19f
Nathan Hartley <3682967+treestryder@users.noreply.github.com>
2026-07-29 22:58:24 -0400

    Revalidating after reflowing.

--- whitewashers/whitewash.ps1
+++ whitewashers/whitewash.ps1
@@ -13,3 +13,20 @@ Pop-Location
 Push-Location "$projectFolder\whitewashers\bin\jeebies_u" -Verbose
 .\jeebies.exe $book > "$projectFolder\whitewashers\_jeebies.txt"
 Pop-Location
+
+
+$lineCount = 0
+$widths = @()
+
+$startLine = 75
+$endLine = 13788
+
+Get-Content $book | ForEach-Object {
+    $lineCount++
+    if ($lineCount -gt $startLine -and $lineCount -lt $endLine -and -not [string]::IsNullOrEmpty($_.Trim())) {
+        $widths += $_.Length
+    }
+}
+
+Write-Host "Line Count: $lineCount"
+Write-Host "Average Width: $($widths | Measure-Object -Average | Select-Object -ExpandProperty Average)"
